11f0ddf36f8a9e686ef137d7a4a63d03a3218e5e
ConsoleCommando <111804621+ConsoleCommando@users.noreply.github.com>
2024-03-06 17:54:41 -0600

    Update dump.sql

--- db/dump.sql
+++ db/dump.sql
@@ -9,3 +9,12 @@ CREATE TABLE `test` (
 
 INSERT INTO rc.test(title, body)
 VALUES('This is a test','This is placeholder text!');
+
+CREATE TABLE Users (
+    username VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    UNIQUE (username)
+);
+
+INSERT INTO Users (username, name, password) VALUES ('admin', 'admin', '$2y$10$54/Xfan9DY9UK0CPH60d6uuYkHkK10gERf6hI9dyIpM25.CPd/rcq');
