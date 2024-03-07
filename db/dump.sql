a2f557c3f21700e030331d3e5e890df30c375d9b
justice <81177526+tPeltier@users.noreply.github.com>
2024-03-07 14:47:02 -0500

    Removing init testing data
    Removed test.php and removed the testing table and its insertions from
    the db

--- db/dump.sql
+++ db/dump.sql
@@ -1,15 +1,5 @@
 USE `rc`;
 
-CREATE TABLE `test` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `title` varchar(100) DEFAULT NULL,
-    `body` text DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO rc.test(title, body)
-VALUES('This is a test','This is placeholder text!');
-
 CREATE TABLE Users (
     username VARCHAR(50) PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
