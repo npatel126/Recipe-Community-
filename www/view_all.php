a2a730f5a1034f36fd5f0159394cd5690a701596
justice <81177526+tPeltier@users.noreply.github.com>
2024-04-02 20:40:49 -0400

    Added view all recipe functionality
    view_all.php is essentially just a copy of the search_recipe.php file,
    except instead of searching for a specific string all recipe entries are
    queried. The search vars were left in to prevent errors from being
    displayed, however they do nothing in the view_all.php file.
    
    A button was added to search_recipe.html to facilitate the call to the
    view_all.php file.
    
    Potential TODO: alter the "view all" ability to a "view favorites" or
    similar as the size of the DB grows.

---
+++ www/view_all.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Recipe Search Results</title>
+    <!--
+    <link href="css/style.css" rel="stylesheet">
+    -->
+</head>
+
+<body>
+
+    <?php
+    // Database connection details
+    $server = "db";
+    $user = "admin";
+    $pw = "pwd";
+    $db = "rc";
+
+    // Check if the script is accessed through a POST request
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Establish a database connection or exit with an error message
+        $connect = mysqli_connect($server, $user, $pw, $db) or die('Could not connect to the database server' . mysqli_connect_error());
+
+        // Sanitize and validate form inputs
+        // These aren't actually doing anything except "burning" any latent input
+        $search = htmlspecialchars($_POST["search"]);
+        $type = $_POST["type"];
+
+        // init vars for query return
+        $title = "";
+        $description = "";
+        $category = "";
+
+        // Search DB for recipe(s) matching description
+        $query = "SELECT title, description, category FROM recipes; ";
+        $stmt = mysqli_prepare($connect, $query);
+    }
+
+    if ($stmt = $connect->prepare($query)) {
+        $stmt->execute();
+        $stmt->bind_result($title, $description, $category);
+    }
+
+    if ($stmt->fetch()) {
+
+        print("<h1> All recipes</h1>");
+
+        print("<table border=1>");
+        print("<tr> <th>Title</th> <th>Description</th> <th>Category</th> <th>View</th> </tr>");
+
+        do {
+            print("<tr><td>$title</td><td>$description</td><td>$category</td><td><a href=\"view_recipe.php?link=$title\">View this Recipe!</a></td></tr>");
+        } while ($stmt->fetch());
+
+        print("</table>");
+    } else {
+        print("<h1> $search returned no results </h1>");
+    }
+
+    // close statement
+    $stmt->close();
+    // close connection with db
+    mysqli_close($connect);
+    ?>
+
+
+    <!-- TODO: this will probably need to change with user sessions -->
+    <form>
+        <p>
+            <input type="submit" formaction="./search_recipe.html" value="Search Again!">
+            <input type="submit" formaction="./index.html" value="Return Home">
+        </p>
+    </form>
+
+</body>
+
+</html>
