85d5c5ff8e15ffeeea103943e7d9b0d8e2848b3e
ConsoleCommando <111804621+ConsoleCommando@users.noreply.github.com>
2024-04-05 20:25:50 -0500

    Update login.php
    Added user sessions.

--- www/login.php
+++ www/login.php
@@ -14,16 +14,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $connect = mysqli_connect($server, $user, $pw, $db) or die('Could not connect to the database server' . mysqli_connect_error());
 
     // Prepare and execute the query
-    $query = "SELECT username, password FROM users WHERE username = ?";
+    $query = "SELECT user_id, username, password FROM users WHERE username = ?";
     $stmt = mysqli_prepare($connect, $query);
     mysqli_stmt_bind_param($stmt, "s", $username);
     mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt, $dbUsername, $dbPassword);
+    mysqli_stmt_bind_result($stmt, $user_id, $dbUsername, $dbPassword);
     mysqli_stmt_fetch($stmt);
     // Verify password
     if ($dbUsername && password_verify($password, $dbPassword)) {
         // Successful login
-        echo "Login successful!";
+         // Start a session
+         session_start();
+         // Set session variables
+         $_SESSION['loggedin'] = true;
+         $_SESSION['username'] = $username;
+         $_SESSION['user_id'] = $user_id;
+
+        header("Location: index.php");
     } else {
         // Failed login, display an error message
         echo "Invalid username or password. Please try again.";
