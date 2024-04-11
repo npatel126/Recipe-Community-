4e66e19c7b28ef51ace4a174d2713e55559fe78f
justice <81177526+tPeltier@users.noreply.github.com>
2024-04-11 18:16:56 -0400

    Init of cookbooks and kitchens files
    They are empty currently

---
+++ www/user_kitchens.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+if (isset($_SESSION["username"]) && $_SESSION["loggedin"] == TRUE) {
+    // echo "Welcome, " . $_SESSION["username"];
+} else {
+    header("Location: index.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Kitchens</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to Your Kitchens, <?php echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest'; ?>!</h1>
+    </header>
+    <button onclick="window.location.href = 'dashboard.php';">Return to dashboard</button>
+</body>
+
+</html>
