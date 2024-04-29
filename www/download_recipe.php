db0e02a0650dbcc915edbd2bc4e1b80a7f5a0304
justice <81177526+tPeltier@users.noreply.github.com>
2024-04-29 03:19:40 -0400

    php script to download json recipe
    The recipe id is passed in and queried, then placed into an key/value
    array and json encoded for raw export.
    
    Users will have to either download or copy out the data depending on
    their browser.
    Its really not ideal at all, but without overhauling the server's
    permissions to r/w files, this enables "exporting" of a recipe
    semi-automatically.
    
    Users will have to manually return to the previous page.
    
    Ideally in the future there will be an in-app way to share recipes
    amongst users.

---
+++ www/download_recipe.php
@@ -0,0 +1,72 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the user is logged in
+if (isset($_SESSION["username"]) && $_SESSION["loggedin"] == TRUE) {
+    //echo "Welcome, " . $_SESSION["username"];
+} else {
+    header("Location: index.php");
+    exit;
+}
+
+$recipe = $_GET['link'];
+
+// Toggle style session variable
+if ($_SESSION['darkmode']) {
+    $style = "css/view_list(dark).css";
+} else {
+    $style = "css/view_list.css";
+}
+
+// Database connection details
+$server = "db";
+$user = "admin";
+$pw = "pwd";
+$db = "rc";
+
+$connect = mysqli_connect($server, $user, $pw, $db) or die('Could not connect to the database server' . mysqli_connect_error());
+
+$search = $_GET['link'];
+// init vars for query return
+$recipe_id = "";
+$name = "";
+$description = "";
+$category = "";
+$cuisine = "";
+$ingredients = "";
+$instructions = "";
+$prep_time = "";
+$cook_time = "";
+$total_time = "";
+$servings = "";
+$creator_id = "";
+$creator_username = "";
+
+// Search DB for recipe(s) matching description
+$query = "SELECT r.recipe_id, r.name, r.description, r.category, r.cuisine, r.ingredients, r.instructions, r.prep_time, r.cook_time, r.total_time, r.servings, r.creator_id, u.username
+            FROM recipes r
+            INNER JOIN users u ON r.creator_id = u.user_id
+            WHERE r.recipe_id = '$search'; ";
+
+$stmt = mysqli_prepare($connect, $query);
+
+if ($stmt = $connect->prepare($query)) {
+    $stmt->execute();
+    $stmt->bind_result($recipe_id, $name, $description, $category, $cuisine, $ingredients, $instructions, $prep_time, $cook_time, $total_time, $servings, $creator_id, $creator_username);
+}
+
+while ($stmt->fetch()) {
+    $export_arr = array('recipe_id' => $recipe_id, 'name' => $name, 'description' => $description, 'category' => $category, 'cuisine' => $cuisine, 'ingredients' => $ingredients, 'instructions' => $instructions, 'prep_time' => $prep_time, 'cook_time' => $cook_time, 'total_time' => $total_time, 'servings' => $servings, 'creator_id' => $creator_id);
+}
+
+// close statement
+$stmt->close();
+
+$export_json = json_encode($export_arr);
+header_remove("Content-Type");
+header_remove("Contenet-Disposition");
+header('Content-Type: application/json');
+$filename = 'Contenet-Disposition: attachement; filename=' . urlencode($name) . '.json';
+header($filename);
+echo $export_json;
